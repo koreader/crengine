82a08f9268fda47e99858b60e473b112b813c5b1
Vadim Lopatin <buggins@fromru.com>
2014-09-10 10:07:43 +0400

    fix unused argument warnings

--- crengine/src/lvtinydom.cpp
+++ crengine/src/lvtinydom.cpp
@@ -2330,6 +2330,7 @@ ldomTextStorageChunk::ldomTextStorageChunk(ldomDataStorageManager * manager, lUI
 	, _type( manager->_type )
 	, _saved(true)
 {
+    CR_UNUSED(compsize);
 }
 
 ldomTextStorageChunk::ldomTextStorageChunk(int preAllocSize, ldomDataStorageManager * manager, lUInt16 index)
