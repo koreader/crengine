8c14036766d001e1e451bdb049a9dcbfaa41b1da
Frans de Jonge <fransdejonge@gmail.com>
2018-03-06 17:58:35 +0100

    info mesage

--- .ci/script.sh
+++ .ci/script.sh
@@ -17,6 +17,7 @@ if [ ! "$file_list" = "$file_list_jq" ]; then
     diff <(echo "$file_list") <(echo "$file_list_jq")
 fi
 
-changed_files=$(git diff --name-only "$TRAVIS_COMMIT_RANGE" |  | grep -E '\.([CcHh]|[ch]pp)$')
-# Run cppcheck on changed files
+changed_files=$(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep -E '\.([CcHh]|[ch]pp)$')
+
+echo "Running cppcheck on $changed_files"
 [ ! -z "$changed_files" ] && cppcheck -j 4 --error-exitcode=2 "$changed_files"
