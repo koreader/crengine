4d5b22fa589409d5e927827b90fc78be61997751
Aleksey Chernov <valexlin@gmail.com>
2019-10-07 00:00:39 +0400

    (Upstream) Glyph caching: optional use of hash table instead of a linked list
    By @virxkane, from https://github.com/buggins/coolreader/pull/111
    5th commit adapted for our non-splitted lvfntman.cpp.
    
    Disabled by default, as it does not provide any noticable
    performance boost.
    
    Original commit message:
    Improved glyph caching: instead of a linked list a hash table is used,
    as result significally faster item's lookup, but, unfortunately,
    this does not affect the overall performance ...
    This feature can be enabled/disabled in crengine/include/crsetup.h via
    symbol USE_GLYPHCACHE_HASHTABLE.
    Earlier, the glyph cache already used a hash table,
    but only when the ligatures feature was turned on,
    and this cache was never cleared. Now this is fixed.

--- crengine/include/crsetup.h
+++ crengine/include/crsetup.h
@@ -42,6 +42,7 @@
 #define USE_GIF                              0
 #define USE_FREETYPE                         0
 #define USE_HARFBUZZ                         0
+#define USE_GLYPHCACHE_HASHTABLE             0
 #define GLYPH_CACHE_SIZE                     0x1000
 #define ZIP_STREAM_BUFFER_SIZE               0x1000
 #define FILE_STREAM_BUFFER_SIZE              0x1000
@@ -53,6 +54,7 @@
 #define USE_GIF                              1
 #define USE_FREETYPE                         1
 #define USE_HARFBUZZ                         1
+#define USE_GLYPHCACHE_HASHTABLE             0
 #define GLYPH_CACHE_SIZE                     0x20000
 #define ZIP_STREAM_BUFFER_SIZE               0x80000
 #define FILE_STREAM_BUFFER_SIZE              0x40000
@@ -76,6 +78,7 @@
 #define GRAY_INVERSE                         0
 #define USE_FREETYPE                         1
 #define USE_HARFBUZZ                         1
+#define USE_GLYPHCACHE_HASHTABLE             0
 
 #ifndef ANDROID
 #ifndef MAC
@@ -113,6 +116,7 @@
 #define USE_HARFBUZZ                         1
 #endif
 #define ALLOW_KERNING                        1
+#define USE_GLYPHCACHE_HASHTABLE             0
 #define GLYPH_CACHE_SIZE                     0x20000
 #define ZIP_STREAM_BUFFER_SIZE               0x80000
 #define FILE_STREAM_BUFFER_SIZE              0x40000
@@ -293,6 +297,10 @@
 
 #ifndef MAX_IMAGE_SCALE_MUL
 #define MAX_IMAGE_SCALE_MUL 2
+#endif
+
+#ifndef USE_GLYPHCACHE_HASHTABLE
+#define USE_GLYPHCACHE_HASHTABLE 0
 #endif
 
 #endif//CRSETUP_H_INCLUDED
