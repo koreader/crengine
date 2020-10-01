36f7541a01806f7ccd6a022f294a864b27e08a60
Aleksey Chernov <valexlin@gmail.com>
2020-10-01 19:51:27 +0200

    (Upstream) Fix sentence selection in TTS
    Continuation of 04030481.
    Don't stumble on "***" paragraph separators.
    (cherry picked from commit e7385e252825458e7a8bdafcaab36c5288247351)

--- crengine/include/lvtinydom.h
+++ crengine/include/lvtinydom.h
@@ -1747,6 +1747,8 @@ public:
     bool prevVisibleWordEndInSentence();
     /// move to next visible word beginning (in sentence)
     bool nextVisibleWordStartInSentence();
+    /// move to end of current word (in sentence)
+    bool thisVisibleWordEndInSentence();
     /// move to next visible word end (in sentence)
     bool nextVisibleWordEndInSentence();
 
