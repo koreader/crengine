fee2b53aa43f538048281e7cc6fbfb919247a17d
Aleksey Chernov <valexlin@gmail.com>
2020-10-27 18:09:51 +0100

    (Upstream) Ignore provided document w/h if negative
    Added a check for the negative width and height of
    the document when rendering, this eliminates freezing
    (infinite recursion) when trying to display a preview
    with insufficient size of program settings window.
    Also changed the check of the minimum and maximum values
    in LVDocView::Resize().

--- crengine/src/lvtinydom.cpp
+++ crengine/src/lvtinydom.cpp
@@ -4532,12 +4532,12 @@ bool ldomDocument::setRenderProps( int width, int dy, bool /*showCover*/, int /*
         _def_font = def_font;
         changed = true;
     }
-    if ( _page_height != dy ) {
+    if ( _page_height != dy && dy > 0 ) {
         CRLog::trace("ldomDocument::setRenderProps() - page height is changed");
         _page_height = dy;
         changed = true;
     }
-    if ( _page_width != width ) {
+    if ( _page_width != width && width > 0 ) {
         CRLog::trace("ldomDocument::setRenderProps() - page width is changed");
         _page_width = width;
         changed = true;
