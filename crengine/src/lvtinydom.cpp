8392c271e558eb9beb722f64762c327a85bfa4e2
poire-z <poire-z@users.noreply.github.com>
2020-10-27 18:10:08 +0100

    Font: fix synthetized italic metrics
    Fake italic metrics, improved in 07bf4a31, were
    still bogus because we were not using the same
    matrix as FT_GlyphSlot_Oblique(). Using the same
    values seems to make quite fine metrics.
    Also remove the other _matrix that wasn't used
    anywhere.

--- crengine/src/lvtinydom.cpp
+++ crengine/src/lvtinydom.cpp
@@ -88,7 +88,7 @@ extern const int gDOMVersionCurrent = DOM_VERSION_CURRENT;
 // increment to force complete reload/reparsing of old file
 #define CACHE_FILE_FORMAT_VERSION "3.05.53k"
 /// increment following value to force re-formatting of old book after load
-#define FORMATTING_VERSION_ID 0x0025
+#define FORMATTING_VERSION_ID 0x0026
 
 #ifndef DOC_DATA_COMPRESSION_LEVEL
 /// data compression level (0=no compression, 1=fast compressions, 3=normal compression)
