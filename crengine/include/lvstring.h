8a322fcdf60d1ba5949c2b9c3c42637d389c7f21
zwim <martin.zwicknagl@kirchbichl.net>
2021-06-29 18:09:47 +0200

    Clean up

--- crengine/include/lvstring.h
+++ crengine/include/lvstring.h
@@ -1052,10 +1052,6 @@ lString32 Utf8ToUnicode( const char * s );
 lString32 Utf8ToUnicode( const char * s, int sz );
 /// converts utf-8 string fragment to wide unicode string
 void Utf8ToUnicode(const lUInt8 * src,  int &srclen, lChar32 * dst, int &dstlen);
-/// counts characters in utf-8 string
-int Utf8CharCount( const lChar8 * str );
-/// counts characters in utf-8 string
-int Utf8CharCount( const lChar8 * str, int len );
 /// decodes path like "file%20name" to "file name"
 lString32 DecodeHTMLUrlString( lString32 s );
 /// truncates string by specified size, appends ... if truncated, prefers to wrap whole words
