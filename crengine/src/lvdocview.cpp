55eb66c947aa7a3d9a95e00e48a76c129d40f93f
Aleksey Chernov <valexlin@gmail.com>
2023-01-05 14:57:52 +0100

    (crengine-ng) Support regular documents inside zip
    LVDocView::loadDocumentInt(): allow to open '*.fb3', '*.epub',
    '*.doc', '*.docx', '*.odt' inside zip-archives.
    
    From crengine-ng commit:
    d0be59c7a24063c6037e8a3bd25fe9f099553a9b

--- crengine/src/lvdocview.cpp
+++ crengine/src/lvdocview.cpp
@@ -4584,6 +4584,7 @@ bool LVDocView::LoadDocument(LVStreamRef stream, bool metadataOnly) {
         }
 #endif
 
+		bool repeat_recursively = false;
         m_arc = LVOpenArchieve( m_stream );
 		if (!m_arc.isNull())
 		{
@@ -4593,12 +4594,7 @@ bool LVDocView::LoadDocument(LVStreamRef stream, bool metadataOnly) {
 			m_container = m_arc;
 			m_doc_props->setInt( DOC_PROP_ARC_FILE_COUNT, m_arc->GetObjectCount() );
 			bool found = false;
-			int htmCount = 0;
-			int fb2Count = 0;
-			int rtfCount = 0;
-			int txtCount = 0;
-			int fbdCount = 0;
-            int pmlCount = 0;
+			int eligibleCount = 0;
 			lString32 defHtml;
 			lString32 firstGood;
 			for (int i=0; i<m_arc->GetObjectCount(); i++)
@@ -4617,17 +4613,27 @@ bool LVDocView::LoadDocument(LVStreamRef stream, bool metadataOnly) {
 							lString32 nm = LVExtractFilenameWithoutExtension( s );
                             if ( nm == "index" || nm == "default" )
 							defHtml = name;
-							htmCount++;
-                        } else if ( s.endsWith(".fb2") ) {
-							fb2Count++;
-                        } else if ( s.endsWith(".rtf") ) {
-							rtfCount++;
-                        } else if ( s.endsWith(".txt") ) {
-							txtCount++;
-                        } else if ( s.endsWith(".pml") ) {
-                            pmlCount++;
-                        } else if ( s.endsWith(".fbd") ) {
-							fbdCount++;
+							eligibleCount++;
+						} else if ( s.endsWith(".fb2") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".fb3") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".epub") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".rtf") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".doc") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".docx") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".odt") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".txt") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".pml") ) {
+							eligibleCount++;
+						} else if ( s.endsWith(".fbd") ) {
+							eligibleCount++;
 						} else {
 							nameIsOk = false;
 						}
@@ -4635,29 +4641,24 @@ bool LVDocView::LoadDocument(LVStreamRef stream, bool metadataOnly) {
 							if ( firstGood.empty() )
                                 firstGood = name;
 						}
-						if ( name.length() >= 5 )
-						{
-							name.lowercase();
-							const lChar32 * pext = name.c_str() + name.length() - 4;
-                            if (!lStr_cmp(pext, ".fb2"))
-                                nameIsOk = true;
-                            else if (!lStr_cmp(pext, ".txt"))
-                                nameIsOk = true;
-                            else if (!lStr_cmp(pext, ".rtf"))
-                                nameIsOk = true;
-						}
-						if ( !nameIsOk )
-						continue;
 					}
 				}
 			}
+			CRLog::debug("In archive found %d suitable files", eligibleCount);
 			lString32 fn = !defHtml.empty() ? defHtml : firstGood;
 			if ( !fn.empty() ) {
+				lString32 s = fn;
+				s = s.lowercase();
+				bool set_codebase = (s.endsWith(".htm") || s.endsWith(".html"));
+				if (s.endsWith(".fb3") || s.endsWith(".epub") || s.endsWith(".doc") ||
+					s.endsWith(".docx") || s.endsWith(".odt"))
+					repeat_recursively = true;
 				m_stream = m_arc->OpenStream( fn.c_str(), LVOM_READ );
 				if ( !m_stream.isNull() ) {
 					CRLog::debug("Opened archive stream %s", LCSTR(fn) );
 					m_doc_props->setString(DOC_PROP_FILE_NAME, fn);
-					m_doc_props->setString(DOC_PROP_CODE_BASE, LVExtractPath(fn) );
+					if (set_codebase)
+						m_doc_props->setString(DOC_PROP_CODE_BASE, LVExtractPath(fn, false));
 					m_doc_props->setString(DOC_PROP_FILE_SIZE, lString32::itoa((int)m_stream->GetSize()));
 					if (!metadataOnly)
 						m_doc_props->setHex(DOC_PROP_FILE_CRC32, m_stream->getcrc32());
@@ -4711,6 +4712,7 @@ bool LVDocView::LoadDocument(LVStreamRef stream, bool metadataOnly) {
 			}
 			delete[] fullbuf;
 #endif
+			// https://wiki.mobileread.com/wiki/TCR : Text compression for Reader, a Psion format
 			LVStreamRef tcrDecoder = LVCreateTCRDecoderStream(m_stream);
 			if (!tcrDecoder.isNull())
 				m_stream = tcrDecoder;
@@ -4731,6 +4733,8 @@ bool LVDocView::LoadDocument(LVStreamRef stream, bool metadataOnly) {
         }
     #endif
 
+		if (repeat_recursively)
+			return LoadDocument(m_stream, metadataOnly);
 		return ParseDocument();
 
 	}
