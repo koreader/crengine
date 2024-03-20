05b3c3d954efd3b4ec73ff2c0319608127955a5c
poire-z <poire-z@users.noreply.github.com>
2024-03-20 16:16:12 +0100

    EPUB: fallback to look for a cover in the first fragment
    If no EPUB3 or EPUB2 cover found, look at the first
    XHTML fragment from the spine: if it contains a single
    image and no text, assume that image can be used as
    a cover.
    Also relax these conditions if the image has alt"=cover":
    it must just be before any text and other images.

--- crengine/include/epubfmt.h
+++ crengine/include/epubfmt.h
@@ -5,7 +5,9 @@
 #include "../include/lvtinydom.h"
 
 bool DetectEpubFormat( LVStreamRef stream );
-bool ImportEpubDocument( LVStreamRef stream, ldomDocument * doc, LVDocViewCallback * progressCallback, CacheLoadingCallback * formatCallback, bool metadataOnly = false );
+bool ImportEpubDocument( LVStreamRef stream, ldomDocument * doc, LVDocViewCallback * progressCallback,
+    CacheLoadingCallback * formatCallback, bool metadataOnly = false,
+    const elem_def_t * node_scheme=NULL, const attr_def_t * attr_scheme=NULL, const ns_def_t * ns_scheme=NULL);
 lString32 EpubGetRootFilePath( LVContainerRef m_arc );
 LVStreamRef GetEpubCoverpage(LVContainerRef arc);
 
