99b080c48754f4a93714a57ce7414dfd8618b017
Bastian Wagner <bastian@baswag.de>
2024-04-03 18:54:37 +0200

    EPUB: add identifiers to doc props

--- crengine/include/lvtinydom.h
+++ crengine/include/lvtinydom.h
@@ -108,6 +108,7 @@ extern const int gDOMVersionCurrent;
 #define DOC_PROP_FILE_CRC32      "doc.file.crc32"
 #define DOC_PROP_CODE_BASE       "doc.file.code.base"
 #define DOC_PROP_COVER_FILE      "doc.cover.file"
+#define DOC_PROP_IDENTIFIERS     "doc.identifiers"
 
 #define DEF_SPACE_WIDTH_SCALE_PERCENT 100
 #define DEF_MIN_SPACE_CONDENSING_PERCENT 50
