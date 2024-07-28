f1203fdba7dd077695f515ac0546801fb023d7d3
Benoit Pierre <benoit.pierre@gmail.com>
2024-07-28 13:42:46 +0200

    mathml: fix compiler warning [-Wunused-variable]

--- crengine/src/mathml.cpp
+++ crengine/src/mathml.cpp
@@ -1944,8 +1944,8 @@ static void fixupMathML( ldomNode * node, bool is_in_script ) {
     const mathml_operator_dict_entry *   infix = NULL;
     const mathml_operator_dict_entry *  prefix = NULL;
     const mathml_operator_dict_entry * postfix = NULL;
-    bool found = getOperatorProperties((const lChar32 *)text.c_str(), infix, prefix, postfix);
-        // printf("%s %d %d %d %d\n", UnicodeToLocal(text).c_str(), found, infix, prefix, postfix);
+    /*bool found =*/ getOperatorProperties((const lChar32 *)text.c_str(), infix, prefix, postfix);
+    // printf("%s %d %d %d %d\n", UnicodeToLocal(text).c_str(), found, infix, prefix, postfix);
 
     // The "form" (prefix/infix/postfix) to be used depends on various things:
     // https://mathml-refresh.github.io/mathml-core/#dictionary-based-attributes
