2a08a9bbe6b223991209fe16b303460013b124d4
spfenwick <stephen@stephenfenwick.nz>
2026-05-18 20:02:06 +1200

    Update NB_IMP_BITS; parens again

--- crengine/include/lvstyles.h
+++ crengine/include/lvstyles.h
@@ -98,7 +98,7 @@ enum css_style_rec_important_bit {
     imp_bit_content,
     imp_bit_cr_hint
 };
-#define NB_IMP_BITS 74 // The number of lines in the enum above: KEEP IT UPDATED.
+#define NB_IMP_BITS 73 // The number of lines in the enum above: KEEP IT UPDATED.
 
 #define NB_IMP_SLOTS    ((NB_IMP_BITS-1)>>5)+1
 // In lvstyles.cpp, we have hardcoded important[0] ... importance[2]
