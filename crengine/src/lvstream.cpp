3bd6a3212337384dc572bf25dcc61f5b08f1e629
Benoit Pierre <benoit.pierre@gmail.com>
2026-07-22 22:19:25 +0200

    minor includes cleanups (#702)

--- crengine/src/lvstream.cpp
+++ crengine/src/lvstream.cpp
@@ -51,18 +51,10 @@ extern "C" {
 #include <sys/stat.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #endif
-
-#ifdef _LINUX
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <dlfcn.h>
-#include <errno.h>
-#endif
-
 
 #ifndef USE_ANSI_FILES
 
