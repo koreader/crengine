507695ced3620bb75ec72fcea2738e27fb75f261
spfenwick <stephen@stephenfenwick.nz>
2026-08-20 19:57:13 +1200

    lvstyles: Refactor hash nested expression

--- crengine/src/lvstyles.cpp
+++ crengine/src/lvstyles.cpp
@@ -47,87 +47,89 @@ lUInt32 calcHash(font_ref_t & f)
 
 lUInt32 calcHash(css_style_rec_t & rec)
 {
-    if ( !rec.hash )
-        rec.hash = ((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((
-         + (lUInt32)rec.important[0]) * 31
-         + (lUInt32)rec.important[1]) * 31
-         + (lUInt32)rec.important[2]) * 31
-         + (lUInt32)rec.importance[0]) * 31
-         + (lUInt32)rec.importance[1]) * 31
-         + (lUInt32)rec.importance[2]) * 31
-         + (lUInt32)rec.display) * 31
-         + (lUInt32)rec.white_space) * 31
-         + (lUInt32)rec.text_align) * 31
-         + (lUInt32)rec.text_align_last) * 31
-         + (lUInt32)rec.text_decoration) * 31
-         + (lUInt32)rec.text_transform) * 31
-         + (lUInt32)rec.hyphenate) * 31
-         + (lUInt32)rec.list_style_type) * 31
-         + (lUInt32)rec.list_style_position) * 31
-         + (lUInt32)rec.letter_spacing.pack()) * 31
-         + (lUInt32)rec.initial_letter.pack()) * 31
-         + (lUInt32)(rec.page_break_before | (rec.page_break_after<<4) | (rec.page_break_inside<<8))) * 31
-         + (lUInt32)rec.vertical_align.pack()) * 31
-         + (lUInt32)rec.font_size.type) * 31
-         + (lUInt32)rec.font_size.value) * 31
-         + (lUInt32)rec.font_style) * 31
-         + (lUInt32)rec.font_weight) * 31
-         + (lUInt32)rec.font_features.pack()) * 31
-         + (lUInt32)rec.font_optical_sizing) * 31
-         + (lUInt32)rec.line_height.pack()) * 31
-         + (lUInt32)rec.color.pack()) * 31
-         + (lUInt32)rec.background_color.pack()) * 31
-         + (lUInt32)rec.width.pack()) * 31
-         + (lUInt32)rec.height.pack()) * 31
-         + (lUInt32)rec.min_width.pack()) * 31
-         + (lUInt32)rec.min_height.pack()) * 31
-         + (lUInt32)rec.max_width.pack()) * 31
-         + (lUInt32)rec.max_height.pack()) * 31
-         + (lUInt32)rec.text_indent.pack()) * 31
-         + (lUInt32)rec.margin[0].pack()) * 31
-         + (lUInt32)rec.margin[1].pack()) * 31
-         + (lUInt32)rec.margin[2].pack()) * 31
-         + (lUInt32)rec.margin[3].pack()) * 31
-         + (lUInt32)rec.padding[0].pack()) * 31
-         + (lUInt32)rec.padding[1].pack()) * 31
-         + (lUInt32)rec.padding[2].pack()) * 31
-         + (lUInt32)rec.padding[3].pack()) * 31
-         + (lUInt32)rec.border_style_top) * 31
-         + (lUInt32)rec.border_style_right) * 31
-         + (lUInt32)rec.border_style_bottom) * 31
-         + (lUInt32)rec.border_style_left) * 31
-         + (lUInt32)rec.border_width[0].pack()) * 31
-         + (lUInt32)rec.border_width[1].pack()) * 31
-         + (lUInt32)rec.border_width[2].pack()) * 31
-         + (lUInt32)rec.border_width[3].pack()) * 31
-         + (lUInt32)rec.border_color[0].pack()) * 31
-         + (lUInt32)rec.border_color[1].pack()) * 31
-         + (lUInt32)rec.border_color[2].pack()) * 31
-         + (lUInt32)rec.border_color[3].pack()) * 31
-         + (lUInt32)rec.background_repeat)*31
-         + (lUInt32)rec.background_position)*31
-         + (lUInt32)rec.background_size[0].pack())*31
-         + (lUInt32)rec.background_size[1].pack())*31
-         + (lUInt32)rec.font_family) * 31
-         + (lUInt32)rec.border_collapse)*31
-         + (lUInt32)rec.border_spacing[0].pack())*31
-         + (lUInt32)rec.border_spacing[1].pack())*31
-         + (lUInt32)rec.orphans) * 31
-         + (lUInt32)rec.widows) * 31
-         + (lUInt32)rec.float_) * 31
-         + (lUInt32)rec.clear) * 31
-         + (lUInt32)rec.direction) * 31
-         + (lUInt32)rec.visibility) * 31
-         + (lUInt32)rec.line_break) * 31
-         + (lUInt32)rec.word_break) * 31
-         + (lUInt32)rec.box_sizing) * 31
-         + (lUInt32)rec.caption_side) * 31
-         + (lUInt32)rec.ruby_position) * 31
-         + (lUInt32)rec.cr_hint.pack()) * 31
-         + (lUInt32)rec.font_name.getHash()
-         + (lUInt32)rec.background_image.getHash()
-         + (lUInt32)rec.content.getHash());
-    return rec.hash;
+    if ( rec.hash )
+        return rec.hash;
+    lUInt32 v = 31;
+    v = v * 31 + (lUInt32)rec.important[0];
+    v = v * 31 + (lUInt32)rec.important[1];
+    v = v * 31 + (lUInt32)rec.important[2];
+    v = v * 31 + (lUInt32)rec.importance[0];
+    v = v * 31 + (lUInt32)rec.importance[1];
+    v = v * 31 + (lUInt32)rec.importance[2];
+    v = v * 31 + (lUInt32)rec.display;
+    v = v * 31 + (lUInt32)rec.white_space;
+    v = v * 31 + (lUInt32)rec.text_align;
+    v = v * 31 + (lUInt32)rec.text_align_last;
+    v = v * 31 + (lUInt32)rec.text_decoration;
+    v = v * 31 + (lUInt32)rec.text_transform;
+    v = v * 31 + (lUInt32)rec.hyphenate;
+    v = v * 31 + (lUInt32)rec.list_style_type;
+    v = v * 31 + (lUInt32)rec.list_style_position;
+    v = v * 31 + (lUInt32)rec.letter_spacing.pack();
+    v = v * 31 + (lUInt32)rec.initial_letter.pack();
+    v = v * 31 + (lUInt32)(rec.page_break_before | (rec.page_break_after<<4) | (rec.page_break_inside<<8));
+    v = v * 31 + (lUInt32)rec.vertical_align.pack();
+    v = v * 31 + (lUInt32)rec.font_size.type;
+    v = v * 31 + (lUInt32)rec.font_size.value;
+    v = v * 31 + (lUInt32)rec.font_style;
+    v = v * 31 + (lUInt32)rec.font_weight;
+    v = v * 31 + (lUInt32)rec.font_features.pack();
+    v = v * 31 + (lUInt32)rec.font_optical_sizing;
+    v = v * 31 + (lUInt32)rec.line_height.pack();
+    v = v * 31 + (lUInt32)rec.color.pack();
+    v = v * 31 + (lUInt32)rec.background_color.pack();
+    v = v * 31 + (lUInt32)rec.width.pack();
+    v = v * 31 + (lUInt32)rec.height.pack();
+    v = v * 31 + (lUInt32)rec.min_width.pack();
+    v = v * 31 + (lUInt32)rec.min_height.pack();
+    v = v * 31 + (lUInt32)rec.max_width.pack();
+    v = v * 31 + (lUInt32)rec.max_height.pack();
+    v = v * 31 + (lUInt32)rec.text_indent.pack();
+    v = v * 31 + (lUInt32)rec.margin[0].pack();
+    v = v * 31 + (lUInt32)rec.margin[1].pack();
+    v = v * 31 + (lUInt32)rec.margin[2].pack();
+    v = v * 31 + (lUInt32)rec.margin[3].pack();
+    v = v * 31 + (lUInt32)rec.padding[0].pack();
+    v = v * 31 + (lUInt32)rec.padding[1].pack();
+    v = v * 31 + (lUInt32)rec.padding[2].pack();
+    v = v * 31 + (lUInt32)rec.padding[3].pack();
+    v = v * 31 + (lUInt32)rec.border_style_top;
+    v = v * 31 + (lUInt32)rec.border_style_right;
+    v = v * 31 + (lUInt32)rec.border_style_bottom;
+    v = v * 31 + (lUInt32)rec.border_style_left;
+    v = v * 31 + (lUInt32)rec.border_width[0].pack();
+    v = v * 31 + (lUInt32)rec.border_width[1].pack();
+    v = v * 31 + (lUInt32)rec.border_width[2].pack();
+    v = v * 31 + (lUInt32)rec.border_width[3].pack();
+    v = v * 31 + (lUInt32)rec.border_color[0].pack();
+    v = v * 31 + (lUInt32)rec.border_color[1].pack();
+    v = v * 31 + (lUInt32)rec.border_color[2].pack();
+    v = v * 31 + (lUInt32)rec.border_color[3].pack();
+    v = v * 31 + (lUInt32)rec.background_repeat;
+    v = v * 31 + (lUInt32)rec.background_position;
+    v = v * 31 + (lUInt32)rec.background_size[0].pack();
+    v = v * 31 + (lUInt32)rec.background_size[1].pack();
+    v = v * 31 + (lUInt32)rec.font_family;
+    v = v * 31 + (lUInt32)rec.border_collapse;
+    v = v * 31 + (lUInt32)rec.border_spacing[0].pack();
+    v = v * 31 + (lUInt32)rec.border_spacing[1].pack();
+    v = v * 31 + (lUInt32)rec.orphans;
+    v = v * 31 + (lUInt32)rec.widows;
+    v = v * 31 + (lUInt32)rec.float_;
+    v = v * 31 + (lUInt32)rec.clear;
+    v = v * 31 + (lUInt32)rec.direction;
+    v = v * 31 + (lUInt32)rec.visibility;
+    v = v * 31 + (lUInt32)rec.line_break;
+    v = v * 31 + (lUInt32)rec.word_break;
+    v = v * 31 + (lUInt32)rec.box_sizing;
+    v = v * 31 + (lUInt32)rec.caption_side;
+    v = v * 31 + (lUInt32)rec.ruby_position;
+    v = v * 31 + (lUInt32)rec.cr_hint.pack();
+    v = v * 31 + (lUInt32)rec.font_name.getHash();
+    v = v * 31 + (lUInt32)rec.background_image.getHash();
+    v = v * 31 + (lUInt32)rec.content.getHash();
+    rec.hash = v;
+    return v;
 }
 
 bool operator == (const css_style_rec_t & r1, const css_style_rec_t & r2)
